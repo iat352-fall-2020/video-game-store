01a8ce4bf1d03013a4c6201587a63595684d7daa
Benedict Wong <bwa44@sfu.ca>
2020-10-30 20:02:08 -0700

    reupload sql because create and update not in the sql file

--- benedict_wong.sql
+++ benedict_wong.sql
@@ -1,31 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 31, 2020 at 12:36 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `benedict_wong`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
+CREATE DATABASE `benedict_wong`;
+USE `benedict_wong`;
 
 CREATE TABLE `cart` (
   `cartID` int(10) NOT NULL,
@@ -52,8 +26,6 @@ CREATE TABLE `customer` (
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`customerID`, `gender`, `email`, `password`, `birthDate`) VALUES
-(1, '', '.cd28b75b5d9e4a20b8dc6566216b17bf.', '.098f6bcd4621d373cade4e832627b4f6.', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -174,6 +146,3 @@ ALTER TABLE `review`
   MODIFY `reviewID` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
